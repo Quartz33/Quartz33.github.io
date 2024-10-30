1d819672e07aaf36cee0806dbc1904a1c9dbc8a0
Quartz33 <joefletch100@gmail.com>
2024-10-30 13:53:28 +0000

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function hover ()
+{
+    document.querySelectorAll('.card').forEach(card => {
+        card.addEventListener('mouseenter', () => {
+            card.style.transform = 'scale(1.05)';
+            card.style.boxShadow = '0px 4px 15px rgba(0, 0, 0, 0.2)';
+        });
+        card.addEventListener('mouseleave', () => {
+            card.style.transform = 'scale(1)';
+            card.style.boxShadow = 'none';
+        });
+    });
+    
+}
